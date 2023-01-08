cfa631dbc24073d9b297d7229d893bbfc834be1a
MirkoDuric <mirkodjuricczv@gmail.com>
2023-01-08 16:33:29 +0100

    JSX-Destructuring finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import cars from "./practice";
 
 function App() {
+  const [honda, tesla] = cars;
+  const {
+    speedStats: { topSpeed: hondaTopSpeed, zeroToSixty: hondaZeroToSixty },
+  } = honda;
+  const {
+    coloursByPopularity: [hondaTopColour, hondabotomColour],
+  } = honda;
+  const {
+    speedStats: { topSpeed: teslaTopSpeed, zeroToSixty: teslaZeroToSixty },
+  } = tesla;
+  const {
+    coloursByPopularity: [teslaTopColour, teslabotomColour],
+  } = tesla;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+    </table>
   );
 }
 
